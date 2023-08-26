16edf1056df603e1aff2b0cc5bc3176d1a301bd8
Casspiann <tarunrana4221>
2023-08-26 14:24:35 +0530

    expess basic

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
 const http = require('http');
-const routes = require('./rout');
+const express = require('express');
+const app = express();
+//const routes = require('./rout');
 
-const server = http.createServer(routes.handler);
-console.log(routes.someText);
-server.listen(4000, () => {
-  console.log('Server is listening on port 4000');
-});
+//const server = http.createServer(routes.handler);
+//console.log(routes.someText);
+app.use((req,res,next)=>{
+  console.log("In the middlewre");
+  next();
+})
+app.use((req,res,next)=>{
+  console.log("In another middlewre");
+  res.send('<h1>Hello from Express!</h1>');
+ // res.send( { key1: 'value' });
+ // next();
+})
+app.listen(3000);
+/*
+const server = http.createServer(app);
+server.listen(3000, () => {
+  console.log('Server is listening on port 3000');
+  
+});*/
 
 /*var http = require('http');
 http.createServer(function (req, res) {

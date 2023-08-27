3043ad7cad7fd9ba19c4f0753ac5ad399a606a5b
Casspiann <tarunrana4221>
2023-08-27 17:09:44 +0530

    handlingdiff routes

--- app.js
+++ app.js
@@ -1,20 +1,28 @@
-const http = require('http');
+//const http = require('http');
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 //const routes = require('./rout');
 
 //const server = http.createServer(routes.handler);
 //console.log(routes.someText);
-app.use((req,res,next)=>{
-  console.log("In the middlewre");
-  next();
+app.use(bodyParser.urlencoded({extended: false}));
+app.use('/add-product',(req,res,next)=>{
+  console.log("In another middlewre");
+  res.send('<form action="/product" method ="POST"><input type="text" name="title"><button type="submit">Add Product</button></input></form>');
+ // res.send( { key1: 'value' });
+ // next();
+});
+app.post('/product',(req,res,next) =>{
+  console.log(req.body);
+  res.redirect('/');
 })
-app.use((req,res,next)=>{
+app.use('/',(req,res,next)=>{
   console.log("In another middlewre");
   res.send('<h1>Hello from Express!</h1>');
  // res.send( { key1: 'value' });
  // next();
-})
+});
 app.listen(3000);
 /*
 const server = http.createServer(app);
